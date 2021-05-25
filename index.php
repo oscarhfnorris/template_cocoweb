a4f2df0ac71568434ee04f8b555ed06af29e5e56
Oscar Norris <57996381+oscarhfnorris@users.noreply.github.com>
2021-05-25 09:36:27 +0100

    testing include tag

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <!-- header imported from header.php -->
+    <?php include("src/page_assets/header.php"); ?>
+    
+
+</body>
+</html>
---
+++ src/page_assets/header.php
@@ -0,0 +1,3 @@
+<html>
+    <h1>HEADER</h1>
+</html>
