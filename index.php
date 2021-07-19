70c04e1db133ca92f00de49bb90915df2cccb7e8
Elenai <60099052+ElenaiDev@users.noreply.github.com>
2021-07-19 14:50:11 +0100

    Updated Index

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<<<<<<< Updated upstream
 <html lang="en">
 
 <?php include("src/page_assets/global_variables.php"); ?>
@@ -8,13 +7,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-=======
 <html>
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="src/css/newstyles.scss" />
+    <link rel="stylesheet" href="/src/css/newstyles.scss"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link
       rel="stylesheet"
@@ -26,7 +24,6 @@
       crossorigin="anonymous"
     ></script>
     <script src="src/js/scroll.js"></script>
->>>>>>> Stashed changes
     <title>Home</title>
   </head>
 
