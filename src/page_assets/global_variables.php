e63741bf682ea2a87f14754eccecb1c9a92d71ae
Elenai <60099052+ElenaiDev@users.noreply.github.com>
2021-07-19 14:34:05 +0100

    Update global_variables.php

--- src/page_assets/global_variables.php
+++ src/page_assets/global_variables.php
@@ -1,3 +1,4 @@
+<html>
 <?php 
 $color_primary = #F82723;
 $color_primary_accent = #D91E6F;
@@ -5,3 +6,4 @@ $color_secondary = #AD1ED9;
 $color_secondary_accent = #8C23F8;
 $color_other = #F02EE3; 
 ?>
+</html>
