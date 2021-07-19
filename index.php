36043517d24097a8d51c5a9fc6f923400c75ae19
Oscar Norris <57996381+oscarhfnorris@users.noreply.github.com>
2021-07-19 14:35:28 +0100

    used include variable for global_variables.php at the top of index.php
    used include variable for global_variables.php at the top of index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
+<?php include("src/page_assets/global_variables"); ?>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- src/page_assets/global_variables.php
+++ src/page_assets/global_variables.php
@@ -4,4 +4,6 @@ $color_primary_accent = #D91E6F;
 $color_secondary = #AD1ED9;
 $color_secondary_accent = #8C23F8;
 $color_other = #F02EE3; 
+
+$null = null;
 ?>
